7b51dc83053fd0559abfb667707c35263e8d68ba
Aditya <192105adityashah@gmail.com>
2024-04-11 23:22:26 +0530

    landing page

--- frontend/src/Pages/Landing/Landing.jsx
+++ frontend/src/Pages/Landing/Landing.jsx
@@ -1,41 +1,38 @@
 import React from "react";
-// import { NavLink } from "react-router-dom";
 
 const Landing = () => {
   return (
     <div>
-      <div>
-        {/* Section 2 */}
-        <section className="px-2 py-32 bg-white md:px-0">
-          <div className="container items-center max-w-6xl px-8 mx-auto xl:px-5">
-            <div className="flex flex-wrap items-center sm:-mx-3">
-              <div className="w-full md:w-1/2 md:px-3">
-                <div className="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
-                  
-                  <p className="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">
-                    Vighnaharata Trust's Shivajirao S. Jondhle College of Engg.
-                    & Technology, Asangaon. Department of Electronics &
-                    Telecommunication Engg.
-                  </p>
-                  
-                </div>
+      <section className="px-4 py-24 bg-white md:px-0">
+        <div className="container mx-auto max-w-6xl">
+          <div className="flex flex-wrap items-center justify-between">
+            <div className="w-full md:w-1/2 md:px-4">
+              <div className="pb-6 space-y-6 md:max-w-md lg:max-w-lg xl:max-w-xl">
+                <p className="text-lg text-gray-700">
+                  Vighnaharata Trust's Shivajirao S. Jondhle College of Engg. & Technology, Asangaon. Department of Electronics & Telecommunication Engg.
+                </p>
               </div>
-              <div className="w-full md:w-1/2">
-                <div className="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl">
-                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkWR7vG_pc_G82S2e3SWD0bmnuoEQJzF75Y0L_TxVY&s" alt="college pic" />
-                </div>
+            </div>
+            <div className="w-full md:w-1/2">
+              <div className="overflow-hidden rounded-md shadow-xl">
+                <img className="w-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkWR7vG_pc_G82S2e3SWD0bmnuoEQJzF75Y0L_TxVY&s" alt="college pic" />
               </div>
             </div>
           </div>
-        </section>
+        </div>
+      </section>
 
-        <div className="w-full flex justify-center items-center" >
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkWR7vG_pc_G82S2e3SWD0bmnuoEQJzF75Y0L_TxVY&s" alt="" />
-        </div>
-        <div className="flex justify-center items-center">
-          <h1>SOme info about College here!</h1>
-        </div>
-       
+      <div className="bg-gray-200 p-4">
+
+      <div className="w-full flex justify-center items-center mt-2">
+        <img className="w-10/12 p-4" src="https://www.jondhleengg.org/images/1.png" alt="" />
+      </div>
+
+      <div className="flex justify-center items-center p-8 m-16">
+        <h1 className="text-xl text-gray-800 max-w-2xl">
+          Established in the year 2007-08 with the intake capacity of 240 students. In 2010 got additional 120 intake. From the Year 2014-15 college got permission for M.E. in Construction Engineering & Management, Energy Resources & Management. The campus spread over 10 acres of land with beautiful buildings. The institute has Administrative office, Classrooms, well-equipped laboratories, Drawing hall, Workshops & Centralized Library facilities.
+        </h1>
+      </div>
       </div>
     </div>
   );
